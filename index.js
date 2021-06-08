41296c5fc43853d40d032936c7828d891ef0983c
masonpyma <masonpyma@tencent.com>
2021-06-08 22:12:29 +0800

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+process.argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`)
+})
+
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+server.start(router.route, handle);
